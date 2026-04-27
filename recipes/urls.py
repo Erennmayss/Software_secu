53119b012d10e28064ff5d734dce948c89f1e4dd
Erennmayss <meloukromaissamalek@gmail.com>
2026-04-27 03:06:40 +0200

    malek

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('regime/', views.regime, name='regime'),
     path('favorite/<int:product_id>/', views.toggle_favorite, name='toggle_favorite'),
 ]
